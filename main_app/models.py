af4e695ebbfd67e968c9652f2554a77fad25eb5e
Harsh Prajapati <harshsp31598@gmail.com>
2023-12-24 14:50:03 -0500

    Initial commit

---
+++ main_app/models.py
@@ -0,0 +1,246 @@
+import datetime
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import UserManager
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = CustomUser(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        assert extra_fields["is_staff"]
+        assert extra_fields["is_superuser"]
+        return self._create_user(email, password, **extra_fields)
+
+
+class Session(models.Model):
+    start_year = models.DateField()
+    end_year = models.DateField()
+
+    def __str__(self):
+        return "From " + str(self.start_year) + " to " + str(self.end_year)
+
+@receiver(post_save, sender=Session)
+def create_or_update_membership(sender, instance, created, **kwargs):
+    """
+    Signal receiver to create or update the corresponding Membership instance
+    when a new Session is saved.
+    """
+    # Define the membership start and end dates for each category
+    year = instance.start_year.year
+    fall_start_date = datetime.date(year, 9, 1)
+    fall_end_date = datetime.date(year, 12, 31)
+    winter_start_date = datetime.date(year, 1, 1)
+    winter_end_date = datetime.date(year, 4, 30)
+    summer_start_date = datetime.date(year, 5, 1)
+    summer_end_date = datetime.date(year, 8, 31)
+
+    # Determine the membership category based on the start_year
+    if fall_start_date <= instance.start_year <= fall_end_date:
+        membership = Membership.FALL + " " + str(year)
+    elif winter_start_date <= instance.start_year <= winter_end_date:
+        membership = Membership.WINTER + " " + str(year)
+    elif summer_start_date <= instance.start_year <= summer_end_date:
+        membership = Membership.SUMMER + " " + str(year)
+    else:
+        # If the start_year doesn't fall within any of the defined ranges,
+        # set membership to None or raise an error, depending on your requirement.
+        membership = None
+
+    # Create or update the Membership instance
+    membership_instance, _ = Membership.objects.get_or_create(session=instance)
+    membership_instance.membership = membership
+    membership_instance.save()
+
+class Membership(models.Model):
+    FALL = 'Fall'
+    WINTER = 'Winter'
+    SUMMER = 'Summer'
+    MEMBERSHIP_CHOICES = [
+        (FALL, 'Fall'),
+        (WINTER, 'Winter'),
+        (SUMMER, 'Summer'),
+    ]
+
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, default=None)
+    membership = models.CharField(max_length=10, choices=MEMBERSHIP_CHOICES, null=True, default=None)
+
+    def __str__(self):
+        return f"{self.session} - {self.get_membership_display()}"
+
+
+class CustomUser(AbstractUser):
+    USER_TYPE = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    GENDER = [("M", "Male"), ("F", "Female")]
+    
+    
+    username = None  # Removed username, using email instead
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(default=1, choices=USER_TYPE, max_length=1)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    profile_pic = models.ImageField()
+    address = models.TextField()
+    fcm_token = models.TextField(default="")  # For firebase notifications
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
+
+
+class Admin(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=True, blank=False)
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING, null=True)
+
+    def __str__(self):
+        return self.admin.last_name + ", " + self.admin.first_name
+
+
+class Staff(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=True, blank=False)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=120)
+    staff = models.ForeignKey(Staff,on_delete=models.CASCADE,)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Attendance(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING)
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class AttendanceReport(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    attendance = models.ForeignKey(Attendance, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class LeaveReportStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class LeaveReportStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeedbackStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeedbackStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class NotificationStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class NotificationStudent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StudentResult(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    test = models.FloatField(default=0)
+    exam = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            Admin.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Staff.objects.create(admin=instance)
+        if instance.user_type == 3:
+            Student.objects.create(admin=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.admin.save()
+    if instance.user_type == 2:
+        instance.staff.save()
+    if instance.user_type == 3:
+        instance.student.save()
